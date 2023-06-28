9c89ca8914b516007eff90cdd9f3db708295af94
Andres Cubillos <andrestj1996@gmail.com>
2023-06-27 20:10:20 -0400

    pruebas unitarias listas.

--- src/stomas/tests/GaudiTest.java
+++ src/stomas/tests/GaudiTest.java
@@ -17,11 +17,16 @@ public class GaudiTest extends TestCase {
     protected void setUp(){
         this._encargado = new Encargado("Encargado", "12345678-9", "Contador", 1997);
         this._sala = new Sala("Sala 1", 4, 20, true, true, _encargado);
+
+        Autor autor = new Autor("Leonardo","da Vinci", "123456789", "Italia");
+        Tamanio tamanio = new Tamanio(120, 90);
+        this._pintura = new Pintura("La Mona Lisa", 1503, autor, Tecnica.Oleo, Genero.Retrato, tamanio, this._sala);
     }
 
     protected void tearDown(){
         this._encargado = null;
         this._sala = null;
+        this._pintura = null;
     }
 
     /*** Pruebas Sala ***/
@@ -36,6 +41,21 @@ public class GaudiTest extends TestCase {
 
     public void testCierreCentralizadoFalse(){
         assertFalse(_sala.cierreCentralizado);
+    }
+
+    public void testCrearPintura_ValoresCorrectos() {
+        assertNotSame("", _pintura.nombre);
+        assertTrue(_pintura.anio > 0);
+        assertNotNull(_pintura.autor);
+        assertNotNull(_pintura.tecnica);
+        assertNotNull(_pintura.genero);
+        assertNotNull(_pintura.tamanio);
+        assertEquals(_sala, _pintura.ubicacion);
+    }
+
+    public void testCrearPintura_UidGenerado() {
+        assertNotNull(_pintura.uid);
+        assertNotSame("", _pintura.uid);
     }
 
     public static void main(String[] args) {
